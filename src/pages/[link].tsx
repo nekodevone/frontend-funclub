f10c64d9771a8002c24c5ec1754eb30302038711
Nekonyx <nekonyx@proton.me>
2022-09-21 11:13:53 +0600

    fix: wait untill router is ready in `[link].tsx`

--- src/pages/[link].tsx
+++ src/pages/[link].tsx
@@ -4,7 +4,8 @@ import { useEffect } from 'react'
 import { Layout } from '../components/layout'
 
 const links: Record<string, string> = {
-  donate: 'https://docs.google.com/document/d/1UXNTbQmOncB2Cd-X0ld6mjF4iSySNWAEmm9rtTFW5Qk/edit',
+  donate:
+    'https://docs.google.com/document/d/1UXNTbQmOncB2Cd-X0ld6mjF4iSySNWAEmm9rtTFW5Qk/edit',
   buy: 'https://docs.google.com/document/d/1kuLrLCTu4NLXZIFYY3OO-EEedQDKAROizsnjbLpDTos/edit',
   discord: 'https://discord.com/invite/MQ2q4nCMYT'
 }
@@ -13,8 +14,12 @@ export const Link: React.FC = () => {
   const router = useRouter()
 
   useEffect(() => {
+    if (!router?.query?.link) {
+      return
+    }
+
     router.push(links[router.query.link as string] ?? '/')
-  }, [router?.query.link])
+  }, [router])
 
   return <Layout title="Переадресация">🐒</Layout>
 }
