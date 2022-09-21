9615ef8038954a12b3858f347e47badc0944dd2c
Nekonyx <nekonyx@proton.me>
2022-09-21 11:36:57 +0600

    refactor: move guilds list to `constants.ts` and store locally their icons

---
+++ src/constants.ts
@@ -0,0 +1,43 @@
+import mainGuildImage from '@/assets/images/guilds/main.png'
+import scpcbmGuildImage from '@/assets/images/guilds/scp-cbm.png'
+import scpslGuildImage from '@/assets/images/guilds/scp-sl.png'
+import ss13GuildImage from '@/assets/images/guilds/ss13.png'
+
+type Guild = {
+  id: string
+  name: string
+  members: number
+  inviteUrl: string
+  image: string
+}
+
+export const guilds: Guild[] = [
+  {
+    id: '433622753350778890',
+    name: 'FUNCLUB',
+    members: 19000,
+    inviteUrl: 'https://discord.gg/MQ2q4nCMYT',
+    image: mainGuildImage.src
+  },
+  {
+    id: '891624095630909493',
+    name: 'FUNCLUB SS13',
+    members: 1000,
+    inviteUrl: 'https://discord.gg/2WAsvv5B5v',
+    image: ss13GuildImage.src
+  },
+  {
+    id: '886375983786127420',
+    name: 'FUNCLUB SCP:SL',
+    members: 6000,
+    inviteUrl: 'https://discord.gg/9VWehX3V7N',
+    image: scpslGuildImage.src
+  },
+  {
+    id: '903733187862200330',
+    name: 'FUNCLUB SCP:CBM',
+    members: 2000,
+    inviteUrl: 'https://discord.gg/ZFPq2PXKTA',
+    image: scpcbmGuildImage.src
+  }
+]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,52 +2,10 @@ import clsx from 'clsx'
 import Link from 'next/link'
 
 import { DiscordCard } from '@/components/discord-card'
+import { Layout } from '@/components/layout'
+import { guilds } from '@/constants'
 
-import { Layout } from '../components/layout'
 import styles from './index.module.css'
-
-type Guild = {
-  id: string
-  name: string
-  members: number
-  image: string
-  invite: string
-}
-
-const guilds: Guild[] = [
-  {
-    id: '433622753350778890',
-    name: 'FUNCLUB',
-    members: 19000,
-    image:
-      'https://cdn.discordapp.com/icons/433622753350778890/46ddb31938500eb21a4eda06eb0959be.png?size=128',
-    invite: 'https://discord.gg/MQ2q4nCMYT'
-  },
-  {
-    id: '891624095630909493',
-    name: 'FUNCLUB SS13',
-    members: 1000,
-    invite: 'https://discord.gg/2WAsvv5B5v',
-    image:
-      'https://cdn.discordapp.com/icons/891624095630909493/8253ddbb47384fd772e6d51a9144ac01.png?size=128'
-  },
-  {
-    id: '886375983786127420',
-    name: 'FUNCLUB SCP:SL',
-    members: 6000,
-    invite: 'https://discord.gg/9VWehX3V7N',
-    image:
-      'https://cdn.discordapp.com/icons/886375983786127420/18f30f55b4164f57c425034b6e55c067.png?size=128'
-  },
-  {
-    id: '903733187862200330',
-    name: 'FUNCLUB SCP:CBM',
-    members: 2000,
-    invite: 'https://discord.gg/ZFPq2PXKTA',
-    image:
-      'https://cdn.discordapp.com/icons/903733187862200330/58f0957fb8f18499eaeab79db150496d.png?size=128'
-  }
-]
 
 const Index: React.FC = () => {
   return (
@@ -102,7 +60,7 @@ const Index: React.FC = () => {
               key={guild.id}
               name={guild.name}
               members={guild.members}
-              invite={guild.invite}
+              invite={guild.inviteUrl}
               image={guild.image}
             />
           ))}
